67a7fa6b9ab5a402375811c090e8b3fc5aa0a586
Sam Rae <samuelrae@gmail.com>
2015-08-27 21:57:31 +0100

    got basic functionality working - can work out length a journey. No error checking or continuous running yet. Also need to deal with plurals/singular. Eg. 1 stops

--- mta.rb
+++ mta.rb
@@ -1,52 +1,42 @@
 subway = {
- N: ['Times Square', '34th', 'n28th', 'n23rd', 'Union Square', 'n8th'],
- L: ['l8th', '6th', 'Union Square', '3rd', '1st'],
- six: ['Grand Central', '33rd', '6_28th', '6_23rd', 'Union Square', 'Astor Place'] 
+ 'N' => ['Times Square', '34th', 'n28th', 'n23rd', 'Union Square', 'n8th'],
+ 'L' => ['l8th', '6th', 'Union Square', '3rd', '1st'],
+ 'six' => ['Grand Central', '33rd', '6_28th', '6_23rd', 'Union Square', 'Astor Place'] 
 }
 
+puts "What line are you starting with #{subway.keys}?"
+start_line = gets.chomp
+puts "You're starting on " + start_line
 
-puts 'What is you start station?'
-puts subway[:N]
-puts subway[:L]
-puts subway[:six]
-start_station = gets
-puts 'Your start station is '+start_station
+puts "What station are you leaving from #{subway[start_line]}?"
+start_station = gets.chomp
+puts "You're leaving from " + start_station
 
-puts 'What is you end station?'
-puts subway[:N]
-puts subway[:L]
-puts subway[:six]
-end_station = gets
-puts 'Your end station is '+end_station
+puts "What line are you arriving on #{subway.keys}?"
+end_line = gets.chomp
+puts "You're ending your journey on " + end_line
 
-if subway[:N].include? start_station
-start_station_line='N'
-elsif subway[:L].include? start_station
-start_station_line='L'
-elsif subway[:six].include? start_station
-start_station_line='six'
-end
+puts "What station are you ending at #{subway[end_line]}?"
+end_station = gets.chomp
+puts "You're ending at " + end_station
 
-puts 'Your start line is ' + start_station_line
-
-if subway[:N].include? end_station
-end_station_line='N'
-elsif subway[:L].include? end_station
-end_station_line='L'
-elsif subway[:six].include? end_station
-end_station_line='six'
-end
-
-puts 'Your end line is '+end_station_line
-
-
-
-
-  
-
-#check to see which array start station is on
-#check to see what array end station is in
 
 #if they are on the same line, find the difference between their indexes
+if start_line === end_line
+  # print 'start station index: '+subway[:start_line].index(start_station)
+  difference = subway[start_line].index(end_station) - subway[end_line].index(start_station)
+  journeyLength = difference.abs
+  Puts 'Your journey will be ' + journeyLength.to_s + 'stops.'
+else
+  difference1 = subway[start_line].index('Union Square') - subway[start_line].index(start_station)
+  puts 'First part of journey is ' + difference1.abs.to_s + ' stops'
+  difference2 = subway[end_line].index(end_station) - subway[end_line].index('Union Square')
+  puts 'Second part of journey is ' + difference2.abs.to_s + ' stops'
+  journeyLength = difference1.abs + difference2.abs
+  puts 'Total length of journey is '+ journeyLength.to_s + ' stops'
+end
 
-#if different check both to see how many stops to union square and add together
+
+
+
+
